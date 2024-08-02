e54b6aa023e58514a36f55b1a54933ac05c03720
wepfen <91220515+wepfen@users.noreply.github.com>
2024-08-02 19:35:00 +0200

    rsa + tests c'est carré mais le prof a carry

--- src/cryptoy/rsa_cipher.py
+++ src/cryptoy/rsa_cipher.py
@@ -1,3 +1,5 @@
+import sys
+
 from math import (
     gcd,
 )
@@ -10,6 +12,7 @@ from cryptoy.utils import (
     str_to_int,
 )
 
+sys.setrecursionlimit(5000)
 
 def keygen() -> dict:
     e = 65537
@@ -18,7 +21,23 @@ def keygen() -> dict:
     # 2. Tire aléatoirement un nombre premier q avec la fonction draw_random_prime
     # 3. Calcul de d, l'inverse de e modulo (p - 1) * (q - 1), avec la fonction modular_inverse
     # 4. Renvoit un dictionnaire { "public_key": (e, p * q), "private_key": d}
-    pass
+
+    p = draw_random_prime()
+    q = draw_random_prime()
+    
+       
+
+    # vérifie qu'ils sont premiers entre eux
+    while gcd(p,q) != 1:
+        q = draw_random_prime()
+
+    n = p*q
+
+    phi = ((p-1) * (q-1))%n 
+    d = pow(e, -1, phi)
+
+
+    return { "public_key": (e, n), "private_key": d}
 
 
 def encrypt(msg: str, public_key: tuple) -> int:
@@ -26,11 +45,19 @@ def encrypt(msg: str, public_key: tuple) -> int:
     # 1. Convertir le message en nombre entier avec la fonction str_to_int
     # 2. Verifiez que ce nombre est < public_key[1], sinon lancer une exception
     # 3. Chiffrez le nombre entier avec pow_mod et les paramètre de la clef publique (e, N)
-    pass
+
+    e = public_key[0]
+    n = public_key[1]
+
+    return pow(str_to_int(msg), e, n)
 
 
 def decrypt(msg: int, key: dict) -> str:
     # Implementez le dechiffrement rsa d'un message avec une clef de la forme { "public_key": (e, p * q), "private_key": d}
     # 1. Utilisez pow_mod avec les paramètres de la clef
     # 2. Convertir l'entier calculé en str avec la fonction int_to_str
-    pass
+    
+    n = key["public_key"][1]
+    d = key["private_key"]
+
+    return int_to_str(pow(msg, d, n))
